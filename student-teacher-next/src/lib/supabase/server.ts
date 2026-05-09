b83dd3970a1e9c3cff6d4e3f67c731b5a21fa6d0
mnkamapeter9919 <mnkamapeter9919@gmail.com>
2026-05-10 00:43:36 +0300

    Auto-update

--- student-teacher-next/src/lib/supabase/server.ts
+++ student-teacher-next/src/lib/supabase/server.ts
@@ -9,11 +9,7 @@ export async function createClient() {
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            cookieStore.set(name, value, options)
-          )
-        },
+        setAll() {},
       },
     }
   )
